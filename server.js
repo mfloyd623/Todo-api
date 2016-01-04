f7446368cdb27ac461c197e69ead3f3eaa152e61
Mike Floyd <mfloyd623@gmail.com>
2016-01-03 19:01:03 -0800

    Add Delete Todos Route

--- server.js
+++ server.js
@@ -56,6 +56,20 @@ app.post('/todos', function(req, res) {
     res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    
+    if (!matchedTodo) {
+        res.status(404).send();
+    } else {
+    
+    todos = _.without(todos, matchedTodo);
+    res.json(matchedTodo)
+    }
+});
+
 app.listen(PORT, function() {
     console.log('Express listenting on port ' + PORT);
 });
