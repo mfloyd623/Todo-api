ac37ada9879a43635961c9d2526508e12334cf12
Mike Floyd <mfloyd623@gmail.com>
2016-01-10 15:35:16 -0800

    Convert Get todos ID to use SQLize

--- server.js
+++ server.js
@@ -38,21 +38,15 @@ app.get('/todos', function(req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
-    
-    // var matchedTodo;
-    
-    // todos.forEach(function(todo) {
-    //     if (todo.id === todoId) {
-    //         matchedTodo = todo;
-    //     }
-    // });
-    
-    if (matchedTodo) {
-        res.json(matchedTodo)
-    } else {
-        res.status(404).send();
-    }
+    db.todo.findById(todoId).then(function(todo) {
+        if(!!todo) {                
+                res.json(todo.toJSON());              
+            } else {
+                res.status(404).send();
+            }
+    }, function(e) {
+        res.status(500).send();
+    });    
 });
 
 // POST /todos
@@ -64,17 +58,6 @@ app.post('/todos', function(req, res) {
     }, function(e) {
         res.status(400).json(e);
     });
-    // if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    //     return res.status(400).send();
-    // }
-    
-    // body.description = body.description.trim();
-    
-    // body.id = todoNextId++;
-    
-    // todos.push(body);
-    
-    // res.json(body);
 });
 
 // DELETE /todos/:id
