1a1e859466ab4f6aeb3726a82229a221b6182f95
Mike Floyd <mfloyd623@gmail.com>
2016-01-16 19:25:22 -0800

    Convert CRDU requests to use current user todos only

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', function(req, res) {
 // GET /todos
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
     var query = req.query;    
-    var where = {};
+    var where = {userId: req.user.get('id')};
     
     if (query.hasOwnProperty('completed') && query.completed === 'true') {
         where.completed = true;
@@ -44,7 +44,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 //GET /todos/:id
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    db.todo.findById(todoId).then(function(todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+            }
+            }).then(function(todo) {
         if(!!todo) {                
                 res.json(todo.toJSON());              
             } else {
@@ -77,7 +82,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
     
     db.todo.destroy({
         where : {
-            id: todoId
+            id: todoId,
+            userId: req.user.get('id')
         }
     }).then(function(rowsDeleted) {
         if (rowsDeleted === 0) {
@@ -107,7 +113,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
         Attributes.description = body.description;
     }         
     
-    db.todo.findById(todoId).then(function(todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+            }
+    }).then(function(todo) {
         if (todo) {
             todo.update(Attributes).then(function(todo) {
             res.json(todo.toJSON());
